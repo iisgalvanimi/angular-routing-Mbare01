102348062ff377ae367277b5f67dde5dcdde3c05
Mbare01 <vita.lorenzo@iisgalvanimi.edu.it>
2020-04-22 15:49:16 +0000

    Artist funzionante

--- Angular-Routing/Routing/src/app/spotify.service.ts
+++ Angular-Routing/Routing/src/app/spotify.service.ts
@@ -1,7 +1,6 @@
-import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { environment } from 'src/environments/environment';
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
   providedIn: 'root'
@@ -9,31 +8,33 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class SpotifyService {
    //url per oauth: https://developer.spotify.com/console/get-search-item/
   //Ottengo il modulo HttpClient
-  constructor(private http: HttpClient) { 
-    const headers = new HttpHeaders({Authorization: environment.oauthToken});
-  }
+  constructor(private http: HttpClient) { }
 
-  searchTrack(query: string) {
-    const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQAVyC-JBWWXO8QgCUE4nUguEeqUfOxcv2edoxFycx5MGdIVWorVA9nu8ef2RNF0byohCbsuFwLTbI8GmpOMExJyEhF5U8zaFBQox_0ZDgbNBerTvN_X9GoVOzxD68_P7JDiXzXYhPKFWqaqUkFG0oDG'
-    });
+    searchTrack(query: string) {
+        const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
+        const headers = new HttpHeaders({Authorization: environment.oauthToken});
 
-    let obsTracks = this.http.get(url, { headers });
-    return obsTracks;
+        let obsTracks = this.http.get(url, { headers });
+        return obsTracks;
  //Ritorno un observable ai componenti che richiedono il servizio
+ 
   }
-
-  getTrack(id: string) {
-    const url = `https://api.spotify.com/v1/tracks/${id}`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer TUO_AUTH'
-    });
+    getTrack(id: string) {
+        const url = `https://api.spotify.com/v1/tracks/${id}`;
+        const headers = new HttpHeaders({Authorization: environment.oauthToken});
     
-    return this.http.get(url, { headers });
+        return this.http.get(url, { headers });
   }
-
-  
+    getArtist(id: string) {
+        const url = `https://api.spotify.com/v1/artists/${id}`;
+        const headers = new HttpHeaders({Authorization: environment.oauthToken});
+    
+        return this.http.get(url, { headers });
+  }
+    getAlbum(id: string) {
+        const url = `https://api.spotify.com/v1/albums/${id}`;
+        const headers = new HttpHeaders({Authorization: environment.oauthToken});
+    
+        return this.http.get(url, { headers });
+  }
 }
--- Angular-Routing/Routing/src/environments/environment.ts
+++ Angular-Routing/Routing/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  oauthToken: "Bearer BQAVyC-JBWWXO8QgCUE4nUguEeqUfOxcv2edoxFycx5MGdIVWorVA9nu8ef2RNF0byohCbsuFwLTbI8GmpOMExJyEhF5U8zaFBQox_0ZDgbNBerTvN_X9GoVOzxD68_P7JDiXzXYhPKFWqaqUkFG0oDG"
+  oauthToken: "Bearer BQDp1IMFvcmJGN8dPJw9_K_svow96B1jt-j_oXp9ArqUsRN5kZ446Z_IM2fiPrW-0Kt-LLGSXMDngC8_oidRcyBhc2zosDdMc37OWq0CNsrD0fvULWIIVH4WqrojMe8XVQdTgP-2appPH2qwftY-pq9H"
 };
