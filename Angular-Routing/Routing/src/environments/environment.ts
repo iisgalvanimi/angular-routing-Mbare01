01533f0eb5977af884fe55dd3e527bbd3ca7063a
Mbare01 <vita.lorenzo@iisgalvanimi.edu.it>
2020-04-23 15:42:15 +0000

    Progetto funzionante

--- Angular-Routing/Routing/src/environments/environment.ts
+++ Angular-Routing/Routing/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  oauthToken: "Bearer BQDp1IMFvcmJGN8dPJw9_K_svow96B1jt-j_oXp9ArqUsRN5kZ446Z_IM2fiPrW-0Kt-LLGSXMDngC8_oidRcyBhc2zosDdMc37OWq0CNsrD0fvULWIIVH4WqrojMe8XVQdTgP-2appPH2qwftY-pq9H"
+  oauthToken: "Bearer BQATevnXIAfyM2Bo0m1qlWmbHdA0b9IDVAg5cX_CcaiDB7TufEirIXcebvapKjabp5EV003n8kILz8i8nC9L7HXjtak1HTVxduKC0cj7OUPS5hOLlrSBZoyKmf8uxbyTRFR_cnUWHwXh7DwzxIPe8vZA"
 };
